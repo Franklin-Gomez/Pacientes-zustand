3c1ccbc650e9cc9463ce41da1e5facfc6a4b2d97
Franklin-Gomez <franklin_043097@hotmail.com>
2024-06-17 16:57:49 -0500

    add first step with useForm : register, handleSubmit and formState errors

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
-
+import PatientForm from "./components/PatientForm"
+import PatientList from "./components/PatientList"
 
 function App() {
 
   return (
     <>
-      <p className=" font-bold"> Desde App </p>
+      <div className="container mx-auto mt-20">
+        <h1 className="font-black text-5xl text-center md:w-2/3 md:mx-auto">
+          Seguimiento de Pacientes {' '}
+          <span className="text-indigo-700">Veterinaia</span>
+        </h1>
+
+
+        <div className="mt-12 md:flex">
+          <PatientForm/>
+          <PatientList/>
+        </div>
+      </div>
     </>
   )
 }
