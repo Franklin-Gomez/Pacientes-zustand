077c0043ee8db4f2647ab3c119c66ae5dc644917
Franklin-Gomez <franklin_043097@hotmail.com>
2024-06-18 16:30:54 -0500

    add two button delete and editar , reset form after add register

--- src/components/PatientDetails.tsx
+++ src/components/PatientDetails.tsx
@@ -14,6 +14,20 @@ export default function PatientDetails( { patient } : PatientDetailsProps) {
             <PatientDetailsItem label='email' data={patient.email} />
             <PatientDetailsItem label='Fecha Alta' data={patient.date.toString()} />
             <PatientDetailsItem label='sintomas' data={patient.symptoms} />
+
+            <div className="flex flex-col lg:flex-row justify-between gap-3 mt-10">
+                <button
+                    type="button"
+                    className="py-2 px-10 bg-indigo-600 hover:bg-indigo-700 text-white font-bold uppercase rounded-lg"
+                >Editar</button>
+
+                <button
+                    type="button"
+                    className="py-2 px-10 bg-indigo-600 hover:bg-indigo-700 text-white font-bold uppercase rounded-lg"
+                >Eliminar</button>
+
+            </div>
         </div>
+
     )
 }
