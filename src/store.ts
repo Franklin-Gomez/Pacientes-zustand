bbe354192d22fc40d2bd828b9860f5e7d9279135
Franklin-Gomez <franklin_043097@hotmail.com>
2024-06-18 11:04:06 -0500

    add first global state

--- src/store.ts
+++ src/store.ts
@@ -1,6 +1,14 @@
 import { create } from "zustand";
+import { Patient } from "./types";
+
+type PatientState = { 
+    patients : Patient[]
+}
 
 // Creando Store
-export const usePatientStore = create (() => ({
+export const usePatientStore = create<PatientState> (() => ({
     // states y funciones que la modifican
+
+    // states
+    patients : []
 }))
