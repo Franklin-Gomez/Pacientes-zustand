a643442cbf8f39a6d2cbe36be87a6ece16359254
Franklin-Gomez <franklin_043097@hotmail.com>
2024-06-18 11:58:19 -0500

    writting in the state

--- src/store.ts
+++ src/store.ts
@@ -1,14 +1,30 @@
 import { create } from "zustand";
-import { Patient } from "./types";
+import { DraftPatient, Patient } from "./types";
+import { v4 as uuidv4 } from "uuid";
 
 type PatientState = { 
     patients : Patient[]
+    addPatient : ( data : DraftPatient ) => void
+}
+
+// agregandole el id
+const createPatient = ( patient : DraftPatient) : Patient => { 
+    return {...patient , id : uuidv4() }
 }
 
 // Creando Store
-export const usePatientStore = create<PatientState> (() => ({
+export const usePatientStore = create<PatientState> (( set) => ({
     // states y funciones que la modifican
 
     // states
-    patients : []
+    patients : [],
+
+    // funciones 
+    addPatient : ( data ) => { 
+        const newPatient = createPatient( data )
+        set ( (state) => ({
+            patients : [...state.patients ,  newPatient ]
+        }))
+    }
+
 }))
