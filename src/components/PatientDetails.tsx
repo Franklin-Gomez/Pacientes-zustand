b6b4d125f40c10146f5035fcdad52f8096f5ae63
Franklin-Gomez <franklin_043097@hotmail.com>
2024-06-19 16:13:30 -0500

    fill out the form to editar

--- src/components/PatientDetails.tsx
+++ src/components/PatientDetails.tsx
@@ -29,7 +29,7 @@ export default function PatientDetails( { patient } : PatientDetailsProps) {
 
                 <button
                     type="button"
-                    className="py-2 px-10 bg-indigo-600 hover:bg-indigo-700 text-white font-bold uppercase rounded-lg"
+                    className="py-2 px-10 bg-red-600 hover:bg-red-700 text-white font-bold uppercase rounded-lg"
                     onClick={() => deletePatient( patient.id )}
                 >Eliminar</button>
 
--- src/components/PatientForm.tsx
+++ src/components/PatientForm.tsx
@@ -2,14 +2,33 @@ import { useForm } from 'react-hook-form'
 import Error from './Error'
 import { DraftPatient } from '../types'
 import { usePatientStore } from '../store'
+import { useEffect } from 'react'
 
 export default function PatientForm() {
 
-    const { addPatient } = usePatientStore()
+    const addPatient = usePatientStore( state => state.addPatient)
+    const activeId = usePatientStore( state => state.activeId)
+    const patient = usePatientStore( state => state.patients)
 
-    const { register , handleSubmit , formState : { errors } , reset  } = useForm<DraftPatient>()
+    const { register , handleSubmit , setValue , formState : { errors } , reset  } = useForm<DraftPatient>()
+
+
+    useEffect(() => {
+
+        // llenado del formulario
+        if( activeId ) { 
+            const activePatient = patient.filter( patient => patient.id == activeId )[0]
+            setValue('name', activePatient.name)
+            setValue('caretaker', activePatient.caretaker)
+            setValue('email', activePatient.email)
+            setValue('date', activePatient.date)
+            setValue('symptoms', activePatient.symptoms)
+        }
+
+    }, [activeId])
 
     const registerPatient = ( data : DraftPatient) => { 
+        // data se pasa en automatico con la informacion del formulario
         addPatient( data )
 
         reset()
