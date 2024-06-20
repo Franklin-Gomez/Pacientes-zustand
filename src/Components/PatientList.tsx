ea0f7d3cc7b0b5e5988eda996811bf1e33941cd4
Franklin-Gomez <franklin_043097@hotmail.com>
2024-06-20 15:38:18 -0500

    add form html

--- src/Components/PatientList.tsx
+++ src/Components/PatientList.tsx
@@ -2,7 +2,12 @@
 export default function PatientList() {
     return (
         <div className="md:w-1/2 lg:w-3/5 md:h-screen overflow-scroll">
-            <h1 className="text-center"> Patient-List</h1>
+            <h2 className="font-black text-3xl text-center">Lista Pacientes</h2>
+    
+            <p className="text-lg mt-5 text-center mb-10">
+                Comienza Agregando pacientes  {''}
+                <span className="text-indigo-600 font-bold">y se mostraran</span>
+            </p>
         </div>
     )
 }
