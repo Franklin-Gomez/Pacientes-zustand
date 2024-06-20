53c7e2796c8d0be0dff30e551aa5293948fbd947
Franklin-Gomez <franklin_043097@hotmail.com>
2024-06-20 09:26:14 -0500

    add roact-toastify

--- src/components/PatientForm.tsx
+++ src/components/PatientForm.tsx
@@ -3,6 +3,7 @@ import Error from './Error'
 import { DraftPatient } from '../types'
 import { usePatientStore } from '../store'
 import { useEffect } from 'react'
+import { toast } from 'react-toastify'
 
 
 export default function PatientForm() {
@@ -34,8 +35,10 @@ export default function PatientForm() {
 
         if ( activeId ) { 
             updatePatient( data )
+            toast.success('Paciente Actualizado Correctamente')
         } else { 
             addPatient( data )
+            toast.success('Paciente Registrado Correctamente')
         }
 
         reset()
